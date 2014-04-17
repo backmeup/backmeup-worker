519a6fb9b9917f4606a7048fc0ad04d440ed07f7
Sebastian Pimminger <sebastian.pimminger@gmail.com>
2014-04-17 15:59:10 +0200

    Start plugin infrastructure in worker

--- backmeup-worker-core/src/main/java/org/backmeup/worker/WorkerCore.java
+++ backmeup-worker-core/src/main/java/org/backmeup/worker/WorkerCore.java
@@ -1,14 +1,35 @@
 package org.backmeup.worker;
 
+import java.util.Queue;
+
+import org.backmeup.job.impl.JobReceiver;
+import org.backmeup.keyserver.client.KeyserverFacade;
+import org.backmeup.keyserver.client.impl.KeyserverClient;
+import org.backmeup.model.BackupJob;
 import org.backmeup.plugin.Plugin;
+import org.backmeup.plugin.osgi.PluginImpl;
+import org.backmeup.service.client.BackmeupServiceFacade;
+import org.backmeup.worker.config.Configuration;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class WorkerCore {
+	private final Logger logger = LoggerFactory.getLogger(WorkerCore.class);
+	
 	private int maxWorkerThreads;
 	private int currentWorkerThreads;
 	private Boolean initialized;
 	private WorkerState currentState;
 	
+	private String pluginsDeploymentDir;
+	private String pluginsTempDir;
+	private String pluginsExportedPackages;
 	private Plugin plugins;
+	
+	private JobReceiver jobReceiver;
+	private Queue<BackupJob> jobQueue;
+	
+	
 	
 	// Getters and setters
 	
@@ -39,13 +60,28 @@ public class WorkerCore {
 	// Constructor
 	
 	public WorkerCore() {
+		pluginsDeploymentDir = Configuration.getProperty("backmeup.osgi.deploymentDirectory");
+		pluginsTempDir = Configuration.getProperty("backmeup.osgi.temporaryDirectory");
+		pluginsExportedPackages = Configuration.getProperty("backmeup.osgi.exportedPackages");
+		plugins = new PluginImpl("deploydir", "tempDir", "expPack");
 		
+		maxWorkerThreads = Integer.parseInt(Configuration.getProperty("backmeup.worker.maxParallelJobs"));
+		
+		
+		setCurrentState(WorkerState.Offline);
 	}
 	
 	// Public Methods
 	
 	public void initialize() {
+		logger.info("Initializing backmeup-worker");
+		Boolean errorsDuringInit = false;
 		
+		plugins.startup();
+		
+		if(!errorsDuringInit) {
+			setCurrentState(WorkerState.Idle);
+		}
 	}
 	
 	public void start() {
@@ -53,7 +89,7 @@ public class WorkerCore {
 	}
 	
 	public void shutdown() {
-		
+		plugins.shutdown();
 	}
 	
 	// Nested classes and enums
