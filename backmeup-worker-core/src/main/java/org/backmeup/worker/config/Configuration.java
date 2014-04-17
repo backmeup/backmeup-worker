5895012613969251ffc7ff86088de9b9d728d280
Sebastian Pimminger <sebastian.pimminger@gmail.com>
2014-04-17 15:55:21 +0200

    Read configuration settings from property file

---
+++ backmeup-worker-core/src/main/java/org/backmeup/worker/config/Configuration.java
@@ -0,0 +1,32 @@
+package org.backmeup.worker.config;
+
+import java.io.IOException;
+import java.util.Properties;
+
+public final class Configuration {
+	private static final Properties properties = new Properties();
+	
+	public final static String PROPERTYFILE = "backmeup-worker.properties";
+	
+	static {
+		try {
+			ClassLoader loader = Thread.currentThread().getContextClassLoader();
+			properties.load(loader.getResourceAsStream(PROPERTYFILE));
+		} catch (IOException e) {
+			throw new ExceptionInInitializerError(e);
+		}
+	}
+
+	public static String getProperty(String key) {
+		return properties.getProperty(key);	
+	}
+	
+	public static String getProperty(String key, String defaultValue) {
+		String value = getProperty(key);
+		if(value != null) {
+			return value;
+		} else {
+			return defaultValue;
+		}
+	}
+}
