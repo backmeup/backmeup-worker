f24e7f7ce4c48dc1f68695d4b1675cce4f9f5cc9
Sebastian Pimminger <sebastian.pimminger@gmail.com>
2014-05-02 11:55:40 +0200

    Fields in model classes have to be named exactly as json attributes that come over the wire Put user data into its own class

--- backmeup-worker-core/src/main/java/org/backmeup/keyserver/model/AuthData.java
+++ backmeup-worker-core/src/main/java/org/backmeup/keyserver/model/AuthData.java
@@ -4,10 +4,10 @@ import java.util.HashMap;
 import java.util.Properties;
 
 public class AuthData {
-  private Long bmuAuthinfoId;
-  private Long bmuUserId;
-  private Long bmuServiceId;
-  private HashMap<String, String> aiData = new HashMap<String, String>();
+  private Long bmu_authinfo_id;
+  private Long bmu_user_id;
+  private Long bmu_service_id;
+  private HashMap<String, String> ai_data = new HashMap<String, String>();
 
   public AuthData() {
     super();
@@ -15,46 +15,46 @@ public class AuthData {
 
   public AuthData(Long bmuAuthinfoId, Long bmuUserId, Long bmuServiceId) {
     super();
-    this.bmuAuthinfoId = bmuAuthinfoId;
-    this.bmuUserId = bmuUserId;
-    this.bmuServiceId = bmuServiceId;
+    this.bmu_authinfo_id = bmuAuthinfoId;
+    this.bmu_user_id = bmuUserId;
+    this.bmu_service_id = bmuServiceId;
   }
 
   public Long getBmuAuthinfoId() {
-    return bmuAuthinfoId;
+    return bmu_authinfo_id;
   }
 
   public void setBmuAuthinfoId(Long bmuAuthinfoId) {
-    this.bmuAuthinfoId = bmuAuthinfoId;
+    this.bmu_authinfo_id = bmuAuthinfoId;
   }
   
   public Long getBmuUserId() {
-    return bmuUserId;
+    return bmu_user_id;
   }
 
   public void setBmuUserId(Long bmuUserId) {
-    this.bmuUserId = bmuUserId;
+    this.bmu_user_id = bmuUserId;
   }
 
   public Long getBmuServiceId() {
-    return bmuServiceId;
+    return bmu_service_id;
   }
 
   public void setBmuServiceId(Long bmuServiceId) {
-    this.bmuServiceId = bmuServiceId;
+    this.bmu_service_id = bmuServiceId;
   }
 
   public HashMap<String, String> getAiData() {
-    return aiData;
+    return ai_data;
   }
 
   public void setAiData(HashMap<String, String> aiData) {
-    this.aiData = aiData;
+    this.ai_data = aiData;
   }
   
   public Properties getAiDataProperties() {
     Properties props = new Properties();
-    props.putAll(this.aiData);
+    props.putAll(this.ai_data);
     return props;
   }
 
--- backmeup-worker-core/src/main/java/org/backmeup/keyserver/model/AuthDataResult.java
+++ backmeup-worker-core/src/main/java/org/backmeup/keyserver/model/AuthDataResult.java
@@ -5,96 +5,68 @@ import java.util.Properties;
 
 import org.backmeup.model.Token;
 
-
 public class AuthDataResult {
+	private Token newToken;
+	private String encryption_pwd;
+	private UserData user;
+	private AuthData[] authinfos;
 
-  private Token newToken;  
-  private String encryptionPwd;
-  
-  public static class UserData {
-    private Long bmuUserId;
-    
+	public AuthDataResult() {
+	}
 
-    public Long getBmuUserId() {
-      return bmuUserId;
-    }
+	public AuthDataResult(UserData user, AuthData[] authInformation) {
+		this.user = user;
 
-    public void setBmuUserId(Long bmuUserId) {
-      this.bmuUserId = bmuUserId;
-    }
+		if (authInformation == null) {
+			this.authinfos = new AuthData[0];
+		} else {
+			this.authinfos = Arrays.copyOf(authInformation,
+					authInformation.length);
+		}
+	}
 
-    public UserData() {
-      super();
-    }
+	public UserData getUser() {
+		return user;
+	}
 
-    public UserData(Long bmuUserId) {
-      super();
-      this.bmuUserId = bmuUserId;
-    }
-  }
+	public void setUser(UserData user) {
+		this.user = user;
+	}
 
-  private UserData user;
-  private AuthData[] authinfos;
+	public AuthData[] getAuthinfos() {
+		return authinfos;
+	}
 
-  public AuthDataResult(UserData user, AuthData[] authInformation) {
-    super();
-    this.user = user;
-    
-    if(authInformation == null) { 
-    	this.authinfos = new AuthData[0]; 
-    } else { 
-    	this.authinfos = Arrays.copyOf(authInformation, authInformation.length); 
-    } 
-  }
+	public void setAuthinfos(AuthData[] authInformation) {
+		if (authInformation == null) {
+			this.authinfos = new AuthData[0];
+		} else {
+			this.authinfos = Arrays.copyOf(authInformation,	authInformation.length);
+		}
+	}
 
-  public AuthDataResult() {
-    super();
-  }
+	public Token getNewToken() {
+		return newToken;
+	}
 
-  
-  public UserData getUser() {
-    return user;
-  }
+	public void setNewToken(Token newToken) {
+		this.newToken = newToken;
+	}
 
-  public void setUser(UserData user) {
-    this.user = user;
-  }
+	public String getEncryptionPwd() {
+		return encryption_pwd;
+	}
 
-  public AuthData[] getAuthinfos() {
-    return authinfos;
-  }
+	public void setEncryptionPwd(String encryptionPwd) {
+		this.encryption_pwd = encryptionPwd;
+	}
 
-  public void setAuthinfos(AuthData[] authInformation) {
-	  if(authInformation == null) { 
-		  this.authinfos = new AuthData[0]; 
-	  } else { 
-		  this.authinfos = Arrays.copyOf(authInformation, authInformation.length); 
-	  }
-  }
-
-  
-  public Token getNewToken() {
-    return newToken;  
-  }
-
-  public void setNewToken(Token newToken) {
-    this.newToken = newToken;
-  }
-
-  public Properties getByProfileId(Long profileId) {
-    for (int i = 0; i < authinfos.length; i++) {
-      if (authinfos[i].getBmuAuthinfoId().equals(profileId)) {
-        return authinfos[i].getAiDataProperties();
-      }
-    }
-    return new Properties();
-  }
-
-  public String getEncryptionPwd() {
-    return encryptionPwd;
-  }
-
-  public void setEncryptionPwd(String encryptionPwd) {
-    this.encryptionPwd = encryptionPwd;
-  }
+	public Properties getByProfileId(Long profileId) {
+		for (int i = 0; i < authinfos.length; i++) {
+			if (authinfos[i].getBmuAuthinfoId().equals(profileId)) {
+				return authinfos[i].getAiDataProperties();
+			}
+		}
+		return new Properties();
+	}
 }
