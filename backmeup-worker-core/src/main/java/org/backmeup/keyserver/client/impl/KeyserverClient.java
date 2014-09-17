cd3c56429708eba13359d3da51d47054007642aa
Sebastian Pimminger <sebastian.pimminger@gmail.com>
2014-09-17 15:02:59 +0200

    Removed unused old code Extracted and renamed constants

--- backmeup-worker-core/src/main/java/org/backmeup/keyserver/client/impl/KeyserverClient.java
+++ backmeup-worker-core/src/main/java/org/backmeup/keyserver/client/impl/KeyserverClient.java
@@ -28,7 +28,11 @@ import org.slf4j.LoggerFactory;
 import com.google.gson.Gson;
 
 public class KeyserverClient implements KeyserverFacade {
-	private final Logger logger = LoggerFactory.getLogger(KeyserverClient.class);
+	private static final String SCHEME_HTTPS = "https";
+
+	private static final String SCHEME_HTTP = "http";
+
+	private final Logger LOGGER = LoggerFactory.getLogger(KeyserverClient.class);
 
 	private static class Result {
 		public HttpResponse response;
@@ -45,22 +49,6 @@ public class KeyserverClient implements KeyserverFacade {
 
 	private final String path;
 
-//	private String keystore;
-//
-//	private String keystoreType;
-//
-//	private String keystorePwd;
-//
-//	private String truststore;
-//
-//	private String truststoreType;
-//
-//	private String truststorePwd;
-//
-//	private Boolean allowAllHostnames;
-//
-//	private SchemeRegistry schemeRegistry;
-
 	public KeyserverClient(String scheme, String host, String path) {
 		this.scheme = scheme;
 		this.host = host;
@@ -68,64 +56,19 @@ public class KeyserverClient implements KeyserverFacade {
 	}
 
 	private HttpClient createClient() {
-		if (scheme.equals("http")) {
+		if (scheme.equals(SCHEME_HTTP)) {
 			return HttpClientBuilder.create().build();
-		} else if (scheme.equals("https")) {
-			/*
-			if (schemeRegistry == null) {
-				try {
-					KeyStore keystore = KeyStore.getInstance(keystoreType != null ? keystoreType : KeyStore.getDefaultType());
-					InputStream keystoreInput = getClass().getClassLoader().getResourceAsStream(this.keystore);
-					if (keystoreInput == null) {
-						logger.error("Can't find keystore file: %s",this.keystore);
-						throw new BackMeUpException(String.format("Configuration specifies keystore \"%s\", but the file does not exist. Please copy the keystore to the resources folder of the project.",this.keystore));
-					}
-
-					keystore.load(keystoreInput, keystorePwd != null ? keystorePwd.toCharArray() : null);
-					keystoreInput.close();
-
-					// Don't throw nullpoint if one of the keystore files couldn't be loaded
-					// Instead, load the truststore, leave it null to rely on cacerts
-					// distributed with the JVM
-					KeyStore truststore = null;
-					if (this.truststore != null) {
-						truststore = KeyStore.getInstance(truststoreType != null ? truststoreType : KeyStore.getDefaultType());
-						InputStream truststoreInput = getClass().getClassLoader().getResourceAsStream(this.truststore);
-						if (truststoreInput == null) {
-							logger.error("Can't find truststore file: %s",this.truststore);
-							throw new BackMeUpException(String.format("Configuration specifies to use truststore \"%s\", but the file does not exist. Please copy the truststore to the resources folder of the project.", this.truststore));
-						}
-
-						truststore.load(truststoreInput, truststorePwd != null ? truststorePwd.toCharArray() : null);
-						truststoreInput.close();
-					}
-					schemeRegistry = new SchemeRegistry();
-					SSLSocketFactory lSchemeSocketFactory = new SSLSocketFactory(
-							SSLSocketFactory.TLS,
-							keystore,
-							keystorePwd,
-							truststore,
-							(SecureRandom) null,
-							allowAllHostnames ? SSLSocketFactory.ALLOW_ALL_HOSTNAME_VERIFIER : SSLSocketFactory.STRICT_HOSTNAME_VERIFIER);
-					schemeRegistry.register(new Scheme("https", 443, lSchemeSocketFactory));
-				} catch (Exception e) {
-					throw new BackMeUpException(e);
-				}
-			}
-			
-			final HttpParams httpParams = new BasicHttpParams();
-			return new DefaultHttpClient( new SingleClientConnManager(schemeRegistry), httpParams);
-			*/
+		} else if (scheme.equals(SCHEME_HTTPS)) {
 			throw new BackMeUpException(String.format("Keyserver scheme \"%s\" not supported", scheme));
 		} else {
-			logger.error("Keyserver scheme not supported: %s", scheme);
+			LOGGER.error("Keyserver scheme not supported: %s", scheme);
 			throw new BackMeUpException(String.format("Keyserver scheme \"%s\" not supported", scheme));
 		}
 	}
 
 	private Result execute(String path, ReqType type, String jsonParams) {
 		HttpClient client = createClient();
-		int port = "http".equals(scheme) ? 80 : 443;
+		int port = SCHEME_HTTP.equals(scheme) ? 80 : 443;
 		String rHost = host;
 		if (host.contains(":")) {
 			String[] sp = host.split(":");
@@ -133,7 +76,7 @@ public class KeyserverClient implements KeyserverFacade {
 			try {
 				port = Integer.parseInt(sp[1]);
 			} catch (Exception ex) {
-				logger.error("", ex);
+				LOGGER.error("", ex);
 			}
 		}
 
@@ -170,7 +113,7 @@ public class KeyserverClient implements KeyserverFacade {
 				try {
 					r.content = new Scanner(response.getEntity().getContent()).useDelimiter("\\A").next();
 				} catch (NoSuchElementException nee) {
-					logger.debug("", nee);
+					LOGGER.debug("", nee);
 				}
 			}
 			return r;
@@ -189,7 +132,7 @@ public class KeyserverClient implements KeyserverFacade {
 		String json = g.toJson(token);
 		Result r = execute(path + "/tokens/data", ReqType.POST, json);
 		if (r.response.getStatusLine().getStatusCode() == 200) {
-			logger.debug("Received token data: " + r.content);
+			LOGGER.debug("Received token data: " + r.content);
 			return g.fromJson(r.content, AuthDataResult.class);
 		}
 		throw new BackMeUpException("Failed to retrieve token data: " + r.content);
--- backmeup-worker-core/src/main/java/org/backmeup/service/client/BackmeupServiceFacade.java
+++ backmeup-worker-core/src/main/java/org/backmeup/service/client/BackmeupServiceFacade.java
@@ -3,15 +3,7 @@ package org.backmeup.service.client;
 import org.backmeup.model.dto.BackupJobDTO;
 
 public interface BackmeupServiceFacade {
-//	Status saveStatus(JobStatus status);
-//	void deleteStatusBefore(Long jobId, Date timeStamp);
-	
 	BackupJobDTO getBackupJob(Long jobId);
-	BackupJobDTO updateBackupJob(BackupJobDTO backupJob);
 	
-//	void saveJobProtocol(String username, Long jobId, JobProtocolDTO protocol);
-//	void deleteJobProtocolByUsername(String username);
-	
-//	void sendEmail(String to, String subject, String message);
-	
+	BackupJobDTO updateBackupJob(BackupJobDTO backupJob);	
 }
