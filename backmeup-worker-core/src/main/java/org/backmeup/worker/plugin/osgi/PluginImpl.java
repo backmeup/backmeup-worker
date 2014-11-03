171e5fee033c2315f37c38e8ae6827a9b5d11c14
Sebastian Pimminger <sebastian.pimminger@gmail.com>
2014-11-03 14:35:24 +0100

    Class name changed in backmeup-model project

--- backmeup-worker-core/src/main/java/org/backmeup/worker/plugin/osgi/PluginImpl.java
+++ backmeup-worker-core/src/main/java/org/backmeup/worker/plugin/osgi/PluginImpl.java
@@ -23,8 +23,8 @@ import org.backmeup.plugin.api.connectors.Datasink;
 import org.backmeup.plugin.api.connectors.Datasource;
 import org.backmeup.plugin.spi.Authorizable;
 import org.backmeup.plugin.spi.Authorizable.AuthorizationType;
-import org.backmeup.plugin.spi.InputBased;
-import org.backmeup.plugin.spi.OAuthBased;
+import org.backmeup.plugin.spi.InputBasedAuthorizable;
+import org.backmeup.plugin.spi.OAuthBasedAuthorizable;
 import org.osgi.framework.BundleContext;
 import org.osgi.framework.BundleException;
 import org.osgi.framework.Constants;
@@ -164,9 +164,9 @@ public class PluginImpl implements Plugin {
     public Authorizable getAuthorizable(String sourceSinkId, AuthorizationType authType) {
 		 switch (authType) {
 	        case OAuth:
-	        	return service(OAuthBased.class, "(name=" + sourceSinkId + ")");
+	        	return service(OAuthBasedAuthorizable.class, "(name=" + sourceSinkId + ")");
 	        case InputBased:
-	        	return service(InputBased.class, "(name=" + sourceSinkId + ")");
+	        	return service(InputBasedAuthorizable.class, "(name=" + sourceSinkId + ")");
 	        default:
 	            throw new IllegalArgumentException("unknown authorization type " + authType);
 		 }
