8c6c2c9a6cd816631c3602e1ffc96058696e7968
Sebastian Pimminger <sebastian.pimminger@gmail.com>
2015-06-02 09:40:52 +0200

    Removed deprecated keyserver client. The client is now located in the backmeup-keyserver project

--- backmeup-worker-core/src/main/java/org/backmeup/keyserver/client/KeyserverFacade.java
+++
@@ -1,8 +0,0 @@
-package org.backmeup.keyserver.client;
-
-import org.backmeup.keyserver.model.AuthDataResult;
-import org.backmeup.model.Token;
-
-public interface KeyserverFacade {
-    AuthDataResult getData(Token token);
-}
